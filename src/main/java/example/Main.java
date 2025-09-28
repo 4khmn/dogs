888cb2d9548aa0a56b0edb4dd8f94e3644ee9718
4khmn <denis-akhmin@yandex.ru>
2025-09-28 23:43:57 +0300

    сделал отображение комментариев

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -4,12 +4,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.eclipse.tags.shaded.org.apache.xpath.operations.Bool;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
@@ -17,30 +19,23 @@ import java.util.Properties;
 
 public class Main extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        URL url = new URL("https://dog.ceo/api/breeds/image/random");
-
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        connection.setRequestMethod("GET");
-
-        int responseCode = connection.getResponseCode();
-
-        if (responseCode==200){
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-            StringBuilder builder = new StringBuilder();
-
-            String line;
-            while((line = reader.readLine()) != null){
-                builder.append(line);
+            String dogUrl = null;
+            String isLiked= null;
+            try {
+                dogUrl = getDog()[0];
+                isLiked = getDog()[1];
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
-            reader.close();
-            ObjectMapper mapper = new ObjectMapper();
-
-            DogResponse dogResponse = mapper.readValue(builder.toString(), DogResponse.class);
-
-            String dogUrl = dogResponse.getMessage();
+            boolean isliked;
+            if (isLiked.equals("true")){
+                isliked = true;
+            }
+            else{
+                isliked = false;
+            }
 
             ArrayList<String> comments;
             try {
@@ -52,9 +47,9 @@ public class Main extends HttpServlet{
             }
             request.setAttribute("comments", comments);
             request.setAttribute("dogUrl", dogUrl);
+            request.setAttribute("isliked", isliked);
             request.getRequestDispatcher("/home.jsp").forward(request, response);
 
-        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
@@ -129,15 +124,16 @@ public class Main extends HttpServlet{
 
         rs.close();
         //вторая табличка с комментариями к собаке
+        if (!message.equals("")) {
+            String Insertprep = "INSERT INTO dog_comments(dog_id, comment) VALUES (?, ?)";
+            PreparedStatement insert = cn.prepareStatement(Insertprep);
 
-        String Insertprep = "INSERT INTO dog_comments(dog_id, comment) VALUES (?, ?)";
-        PreparedStatement insert = cn.prepareStatement(Insertprep);
-
-        insert.setInt(1, id);
-        insert.setString(2, message);
-        insert.executeUpdate();
-        System.out.println("Комментарий добавлен");
-        insert.close();
+            insert.setInt(1, id);
+            insert.setString(2, message);
+            insert.executeUpdate();
+            System.out.println("Комментарий добавлен");
+            insert.close();
+        }
         stmt.close();
         cn.close();
     }
@@ -167,8 +163,80 @@ public class Main extends HttpServlet{
         select.executeQuery();
         ResultSet rs = select.executeQuery();
         while(rs.next()){
-            comments.add(rs.getString("comment"));
+            if (!rs.getString("comment").equals("")) {
+                comments.add(rs.getString("comment"));
+            }
         }
         return comments;
+    }
+
+
+    private String[] getDog() throws ClassNotFoundException, SQLException, IOException {
+        Properties props = new Properties();
+        try(InputStream in = getClass().getClassLoader().getResourceAsStream("config.properties")){
+            props.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        String urlD =props.getProperty("db.url");
+        String login = props.getProperty("db.login");
+        String password = props.getProperty("db.password");
+        Class.forName("org.postgresql.Driver");
+        Connection cn = DriverManager.getConnection(urlD, login, password);
+        Statement stmt = cn.createStatement();
+        String selectcount = "SELECT COUNT(*) FROM dogs";
+        PreparedStatement countt = cn.prepareStatement(selectcount);
+
+        ResultSet rs = countt.executeQuery();
+        int count=0;
+        String dog[] = new String[2];
+        if (rs.next()){
+            count=rs.getInt(1);
+        }
+
+        if (count<20){
+            URL url = new URL("https://dog.ceo/api/breeds/image/random");
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode==200) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                StringBuilder builder = new StringBuilder();
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+                reader.close();
+                ObjectMapper mapper = new ObjectMapper();
+
+                DogResponse dogResponse = mapper.readValue(builder.toString(), DogResponse.class);
+
+                dog[0] = dogResponse.getMessage();
+                dog[1]="false";
+            }
+        }
+        else{
+            String rand = "SELECT * FROM dogs ORDER BY RANDOM() LIMIT 1";
+            PreparedStatement random = cn.prepareStatement(rand);
+            ResultSet rs1 = random.executeQuery();
+            if (rs1.next()){
+                dog[0] = rs1.getString("url");
+                boolean isliked = rs1.getBoolean("liked");
+                if (isliked == true){
+                    dog[1] = "true";
+                }
+                else{
+                    dog[1]="false";
+                }
+            }
+        }
+
+        return dog;
     }
 }
